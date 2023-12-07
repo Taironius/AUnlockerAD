e699f30a163fa12ad99b6d7ce98d22c6144a4d92
Kenedy Jardim <kenedydacosta@gmail.com>
2023-12-06 23:55:08 -0300

    simplificadondo o codigo

--- AUnlockerAD.ps1
+++ AUnlockerAD.ps1
@@ -17,20 +17,6 @@ if ($value -ne 0){
                 write-host $i.tostring() " Foi desbloqueado" -ForegroundColor green -backgroundcolor black
             }
         }
-    }elseif($value -lt 2){
-        write-host "Quantidade de contas Bloqueadas: $value" -ForegroundColor white -BackgroundColor red
-        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
-        foreach($i in $queryname){
-            $user = Get-ADUser $i -Properties MemberOf
-            if ($groupDN -in $user.MemberOf){
-                Unlock-ADAccount -Identity $i
-                write-host $i.tostring() " Foi desbloqueado" -ForegroundColor green -backgroundcolor black
-            }
-        }    
     }
-    
-}elseif ($value -eq 0){
-    write-host "Quantidade de contas Bloqueadas: $value" -ForegroundColor white -BackgroundColor red
-        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
 }
 #Read-Host -Prompt "Press Enter to exit"
