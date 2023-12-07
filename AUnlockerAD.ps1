2ee2250b9e203ffc9e2aa47cf9c1427ffdc0d5aa
Kenedy Jardim <kenedydacosta@gmail.com>
2023-12-06 21:39:23 -0300

    simplificado, retirado a log txt e traduzido as mensagens do prompt

--- AUnlockerAD.ps1
+++ AUnlockerAD.ps1
@@ -8,49 +8,29 @@ $count = $queryname | measure
 $value = $count.count
 if ($value -ne 0){
     if ($value -gt 1){
-        write-host "The number of Accounts locked out are $value" -ForegroundColor white -BackgroundColor red
-        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
-        write-host "Users locked are $querynamed"  -ForegroundColor white -BackgroundColor red
-        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
-        write-host "$value Unlock Operations were done" -ForegroundColor green -backgroundcolor black
-        $value.tostring() + " Accounts are locked out at " + ($date) | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-        "$querynamed" + " were locked out at " | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-        "$querynamed" + " have been unlocked on " + ($date) | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-        "`n" | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
+        write-host "Quantidade de contas Bloqueadas: $value" -ForegroundColor white -BackgroundColor red
         write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
         foreach($i in $queryname){
             $user = Get-ADUser $i -Properties MemberOf
             if ($groupDN -in $user.MemberOf){
                 Unlock-ADAccount -Identity $i
-                write-host $i.tostring() " was unlocked" -ForegroundColor green -backgroundcolor black
+                write-host $i.tostring() " Foi desbloqueado" -ForegroundColor green -backgroundcolor black
             }
         }
     }elseif($value -lt 2){
-        write-host "The number of Accounts locked is are $value" -ForegroundColor white -BackgroundColor red
-        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
-        write-host "User locked is $querynamed"  -ForegroundColor white -BackgroundColor red
-        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
-        write-host "$value Unlock Operations were done" -ForegroundColor green -backgroundcolor black
-        $value.tostring() + " Accounts are locked out at " + ($date) | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-        "$querynamed" + " was locked out at " | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-        "$querynamed" + " has been unlocked on " + ($date) | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-        "`n" | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
+        write-host "Quantidade de contas Bloqueadas: $value" -ForegroundColor white -BackgroundColor red
         write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
         foreach($i in $queryname){
             $user = Get-ADUser $i -Properties MemberOf
             if ($groupDN -in $user.MemberOf){
                 Unlock-ADAccount -Identity $i
-                write-host $i.tostring() " was unlocked" -ForegroundColor green -backgroundcolor black
+                write-host $i.tostring() " Foi desbloqueado" -ForegroundColor green -backgroundcolor black
             }
         }    
     }
     
 }elseif ($value -eq 0){
-    write-host "The number of Account locked out is $value" -ForegroundColor green -backgroundcolor black
-    write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
-    write-host "$value Unlock Operation was done" -ForegroundColor green -backgroundcolor black
-    $value.tostring() + " Accounts are locked out at " + ($date) | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-    "No User has been unlocked" + " has been unlocked on " + ($date) | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
-    "`n" | Out-File -filepath C:\Users\Administrator\Desktop\logs\Alog.txt -Append
+    write-host "Quantidade de contas Bloqueadas: $value" -ForegroundColor white -BackgroundColor red
+        write-host "-------------------------------------------------------" -ForegroundColor black -BackgroundColor yellow
 }
 #Read-Host -Prompt "Press Enter to exit"
